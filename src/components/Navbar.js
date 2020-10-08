7218ac531e3218b540730b835c59a42e1cd0f007
Masnoon Junaid <masnoon20@gmail.com>
2020-10-08 12:13:40 +0530

    app components

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../App.css"
+import { Link } from 'react-router-dom';
+import styled from 'styled-components'
+
+//declaring class based component
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from Navbar</h1>
+      </div>
+    )
+  }
+}
+
+
+export default Navbar;
---
+++ src/components/Searchbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Hello from SearchBar</h1>
+      </div>
+    )
+  }
+}
+
+export default SearchBar
